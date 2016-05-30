c055723ee122c820eb947a8c8cf6d1be622e641b
Souriyo <souriyo.soumpholphakdy@axa.com.sg>
2016-05-31 00:44:04 +0800

    Add files via upload

---
+++ AXA_Souriyo_kaggle_titanic.py
@@ -0,0 +1,11 @@
+import csv as csv
+import numpy as np
+
+csv_file_object = csv.reader(open('C:/Users/AISSSOU/Desktop/data science/Analytics_Prework_part2/exo/data/train.csv', 'rb')) 
+header = csv_file_object.next() 
+data=[] 
+
+for row in csv_file_object:
+    data.append(row)
+data = np.array(data) 
+
