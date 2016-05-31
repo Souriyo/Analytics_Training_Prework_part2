ef6c6f203bff686d246b6fc6ec9612206a64bf94
Souriyo <souriyo.soumpholphakdy@axa.com.sg>
2016-05-31 23:23:17 +0800

    Add files via upload

--- AXA_Souriyo_kaggle_titanic.py
+++ AXA_Souriyo_kaggle_titanic.py
@@ -7,9 +7,15 @@ import numpy as np
 test_file = open('C:/Users/AISSSOU/Desktop/data science/Analytics_Prework_part2/exo/data/test.csv', 'rb')
 test_file_object = csv.reader(test_file)
 header = test_file_object.next()
-data=[]
 
-#put csv data in data[]
-for row in test_file_object:
-    data.append(row)
-data = np.array(data) 
+prediction_file = open("genderbasedmodel.csv", "wb")
+prediction_file_object = csv.writer(prediction_file)
+
+prediction_file_object.writerow(["PassengerId", "Survived"])
+for row in test_file_object:       # For each row in test.csv
+    if row[3] == 'female' and row[1] == '1':
+        prediction_file_object.writerow([row[0],'1'])    # predict 1
+    else:                              # or else if male,       
+        prediction_file_object.writerow([row[0],'0'])    # predict 0
+test_file.close()
+prediction_file.close()
