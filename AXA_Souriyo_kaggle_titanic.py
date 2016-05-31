3b5ceccb912ba39501f797110789e77290771869
Souriyo <souriyo.soumpholphakdy@axa.com.sg>
2016-05-31 22:09:56 +0800

    Add files via upload

--- AXA_Souriyo_kaggle_titanic.py
+++ AXA_Souriyo_kaggle_titanic.py
@@ -9,3 +9,36 @@ for row in csv_file_object:
     data.append(row)
 data = np.array(data) 
 
+#conversion passenger class  to float
+data[0::,2].astype(np.float)
+
+# The size() function counts how many elements are in
+# in the array and sum() (as you would expects) sums up
+# the elements in the array.
+
+number_passengers = np.size(data[0::,1].astype(np.float))
+number_survived = np.sum(data[0::,1].astype(np.float))
+proportion_survivors = number_survived / number_passengers
+
+
+women_only_stats = data[0::,4] == "female" # This finds where all 
+                                           # the elements in the gender
+                                           # column that equals “female”
+men_only_stats = data[0::,4] != "female"   # This finds where all the 
+                                           # elements do not equal 
+                                           # female (i.e. male)
+
+
+# Using the index from above we select the females and males separately
+women_onboard = data[women_only_stats,1].astype(np.float)     
+men_onboard = data[men_only_stats,1].astype(np.float)
+
+# Then we finds the proportions of them that survived
+proportion_women_survived = \
+                       np.sum(women_onboard) / np.size(women_onboard)  
+proportion_men_survived = \
+                       np.sum(men_onboard) / np.size(men_onboard) 
+
+# and then print it out
+print 'Proportion of women who survived is %s' % proportion_women_survived
+print 'Proportion of men who survived is %s' % proportion_men_survived
